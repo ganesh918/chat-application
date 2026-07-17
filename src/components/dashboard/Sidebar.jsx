6a93178e3a657a47c4f5425557076bb4b16c730a
Ganesh Kumar Reddy <thatigutlaganesh000@gmail.com>
2026-07-17 22:23:52 +0530

    Updated project

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -18,38 +18,27 @@ function Sidebar() {
       </h2>
 
       <ul>
+  <li>
+    <Link to="/" className="sidebar-link active">
+      <FaComments />
+      <span>Chats</span>
+    </Link>
+  </li>
 
-        <li>
-          <Link
-            to="/dashboard"
-            className="sidebar-link"
-          >
-            <FaComments />
-            <span>Chats</span>
-          </Link>
-        </li>
+  <li>
+    <Link to="/profile" className="sidebar-link">
+      <FaUser />
+      <span>Profile</span>
+    </Link>
+  </li>
 
-        <li>
-          <Link
-            to="/profile"
-            className="sidebar-link"
-          >
-            <FaUser />
-            <span>Profile</span>
-          </Link>
-        </li>
-
-        <li>
-          <Link
-            to="/settings"
-            className="sidebar-link"
-          >
-            <FaCog />
-            <span>Settings</span>
-          </Link>
-        </li>
-
-      </ul>
+  <li>
+    <Link to="/settings" className="sidebar-link">
+      <FaCog />
+      <span>Settings</span>
+    </Link>
+  </li>
+</ul>
 
     </div>
   );
