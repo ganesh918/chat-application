1a1f96606919a73c6b5dbbcef39e46b10107252c
Ganesh Kumar Reddy <thatigutlaganesh000@gmail.com>
2026-07-21 17:52:37 +0530

    Updated project files

--- src/components/chat/ChatHeader.jsx
+++ src/components/chat/ChatHeader.jsx
@@ -31,14 +31,20 @@ function ChatHeader({
           <div>
             <h3>{user.name}</h3>
 
-            <small className="typing-status">
-              Typing
-              <span className="typing-dots">
-                <span></span>
-                <span></span>
-                <span></span>
-              </span>
-            </small>
+            {user.name === "Karthik Kumar" ? (
+              <small className="typing-status">
+                Typing
+                <span className="typing-dots">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </span>
+              </small>
+            ) : (
+              <small>
+                {user.online ? "Online" : "Offline"}
+              </small>
+            )}
           </div>
         </div>
       </div>
